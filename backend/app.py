312feac910d0c64794efce634fde6fe19d66c3b2
Vidhya2101 <vidhyakrao21@gmail.com>
2025-05-19 22:20:49 +0530

    changes in app.py

--- backend/app.py
+++ backend/app.py
@@ -191,4 +191,5 @@ def serve_login():
     return send_from_directory('static', 'login.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
